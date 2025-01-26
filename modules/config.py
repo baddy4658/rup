5b69dd516d123e67c476d7c2dec96769bfb3cb91
baddy4658 <Aviothic317@gmail.com>
2025-01-26 19:00:20 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "Luminant ❤️"
+BOT_TOKEN = "7756940226:AAG7491TOD6Xnum97kZ8j8GS58l2bugG3xQ"
+API_ID = 1554191921863002
+API_HASH = "fbf0d590575bbaaad9d773d04c8872e4"
+MONGO_URI = "mongodb+srv://himanshubainsla1:7AT7w4BOzPMW7QRF@cluster0.8wsag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
